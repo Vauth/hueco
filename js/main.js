9bc2a044a05e4620330d91209af0806c7ddb32ce
Vauth <122077080+Vauth@users.noreply.github.com>
2024-07-10 12:20:20 +0330

    DYNAMIC HOST final

--- js/main.js
+++ js/main.js
@@ -44,7 +44,7 @@ var configs = (function () {
         accesible_cores: "Accessible cores",
         language: "Language",
         value_token: "<value>",
-        host: window.location.hostname, // "hueco.link" => Replace this with your host
+        host: window.location.hostname.split('.')[0], // "hueco.link" => Replace this with your host
         user: "root",
         is_root: true,
         type_delay: 2
